77d34d3bbe611b4d47871cabffebb083d5d0523b
rashad2024 <152859844+rashad2024@users.noreply.github.com>
2025-05-03 21:02:16 +0600

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -11,7 +11,7 @@ import "@/styles/pages/home.scss";
 export default function HeaderPage({
   pageType,
 }: {
-  pageType?: string | undefined;
+  pageType?: string;
 }) {
   return (
     <AuthGuard>
